64721e02c69757439d0cadf963d7f9c1d85d264c
Axect <edeftg@gmail.com>
2017-07-05 22:39:53 +0900

    Fix align

--- Tutorial/pointer/cmd/main.go
+++ Tutorial/pointer/cmd/main.go
@@ -14,11 +14,14 @@ func main() {
 	start := time.Now()
 	fmt.Println(pointer.SumList(array))
 	elapsed := time.Since(start)
+
 	start2 := time.Now()
 	fmt.Println(pointer.SumPointer(&array))
 	elapsed2 := time.Since(start2)
+
 	start3 := time.Now()
 	fmt.Println(pointer.SumSlice(slice))
 	elapsed3 := time.Since(start3)
+
 	fmt.Println("Array:", elapsed, "Pointer:", elapsed2, "Slice:", elapsed3)
 }
