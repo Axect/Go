4e1d61fc3dcacdedbae52e78196d2ca7ed274c60
Axect <edeftg@gmail.com>
2017-07-07 15:30:39 +0900

    Add type

---
+++ Tutorial/type/type.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type user struct {
+	name       string
+	email      string
+	ext        int
+	privileged bool
+}
+
+type admin struct {
+	person user
+	level  string
+}
+
+var tom = admin{
+	person: user{
+		name:       "tom",
+		email:      "abcd@naver.com",
+		ext:        123,
+		privileged: true,
+	},
+	level: "super",
+}
+
+func main() {
+	fmt.Println(tom.person)
+}
