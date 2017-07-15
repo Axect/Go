6e3ca5cc970139c240b46e3d032d6db931a2d0ee
Axect <edeftg@gmail.com>
2017-07-16 01:03:28 +0900

    Add Mean and New type

--- CS/prob2/sort.go
+++ CS/prob2/sort.go
@@ -30,6 +30,50 @@ func (p PhysicsList) Len() int           { return len(p) }
 func (p PhysicsList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 func (p PhysicsList) Less(i, j int) bool { return p[i].Score < p[j].Score }
 
+// Mean is mean
+func (p PhysicsList) Mean() float64 {
+	s := 0
+	l := p.Len()
+	for _, elem := range p {
+		s += elem.Score
+	}
+	return float64(s) / float64(l)
+}
+
+// NewContainer contains rank
+type NewContainer struct {
+	Inform PhysicsTest
+	Rank   int
+}
+
+// =============================================================================
+// Methods for NewContainer
+// =============================================================================
+func (Elem NewContainer) String() string {
+	return fmt.Sprintf("%s, %d, %d\n", Elem.Inform.Name, Elem.Inform.Score, Elem.Rank)
+}
+
+// AssignRank - rank
+func (Elem *NewContainer) AssignRank(n int) { Elem.Rank = n }
+
+// AssignInform - inform
+func (Elem *NewContainer) AssignInform(value PhysicsTest) { Elem.Inform = value }
+
+// Rank ranks list
+func Rank(SortedArray PhysicsList) []NewContainer {
+	length := SortedArray.Len()
+	Temp := make([]NewContainer, length, length)
+	for i := range SortedArray {
+		Temp[i].AssignRank(i + 1)
+		Temp[i].AssignInform(SortedArray[i])
+	}
+	return Temp
+}
+
+// =============================================================================
+// Action
+// =============================================================================
+
 // DoSort do sort
 func DoSort() {
 	fmt.Println("---------------------------------------")
@@ -50,5 +94,7 @@ func DoSort() {
 		{"Planck", 52},
 	}
 	sort.Sort(sort.Reverse(A))
-	fmt.Println(A)
+	Result := Rank(A)
+	fmt.Println(Result)
+	fmt.Println(A.Mean())
 }
