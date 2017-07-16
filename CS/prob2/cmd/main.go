df86d9940c963714e4625a88d406cd5bbe229239
Axect <edeftg@gmail.com>
2017-07-16 17:15:47 +0900

    Make RSP

--- CS/prob2/cmd/main.go
+++ CS/prob2/cmd/main.go
@@ -5,4 +5,5 @@ import "github.com/Axect/Go/CS/prob2"
 func main() {
 	prob2.DoRoll()
 	prob2.DoSort()
+	prob2.DoRSP()
 }
---
+++ CS/prob2/rsp.go
@@ -0,0 +1,87 @@
+package prob2
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// =============================================================================
+// Type & Method for RSP
+// =============================================================================
+
+// Score is type for scoring
+type Score struct {
+	player int
+	enemy  int
+	draw   int
+}
+
+// Judge for Score
+func (s *Score) Judge(x, y int) {
+	// At least one of them put rock
+	if x*y == 0 {
+		if x > y {
+			s.player++
+		} else if x < y {
+			s.enemy++
+		} else {
+			s.draw++
+		}
+	} else if x*y < 0 { // At least one of them put Scissor
+		if x < 0 {
+			s.player++
+		} else {
+			s.enemy++
+		}
+	} else {
+		s.draw++
+	}
+}
+
+// Play is method for Score
+func (s *Score) Play(n int) {
+	for i := 0; i < n; i++ {
+		fmt.Println("가위, 바위, 보를 시작합니다. 가위, 바위, 보!")
+		var player string
+		fmt.Scan(&player)
+		enemy := AI()
+		s.Judge(RSP[player], RSP[enemy])
+	}
+}
+
+// =============================================================================
+// Map for RSP
+// =============================================================================
+
+// RSP for rsp
+var RSP = map[string]int{
+	"가위": -1,
+	"바위": 0,
+	"보":  1,
+}
+
+// =============================================================================
+// Make Random Engine for RSP
+// =============================================================================
+
+//AI for Rsp
+func AI() string {
+	rand.Seed(int64(time.Now().Nanosecond()))
+	number := rand.Intn(3)
+	List := [3]string{"가위", "바위", "보"}
+	return List[number]
+}
+
+//DoRSP !
+func DoRSP() {
+	fmt.Println("---------------------------------------")
+	fmt.Println("Rock Scissors Paper!")
+	fmt.Println("---------------------------------------")
+	var n int
+	fmt.Println("How many times you want to play RSP?")
+	fmt.Scanln(&n)
+	var Count Score
+	Count.Play(n)
+	fmt.Printf("Player: %v, Computer: %v, Draw:%v\n", Count.player, Count.enemy, Count.draw)
+}
