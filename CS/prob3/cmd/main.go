28754298c76e7347c7e5a5cfcf827229d23ca9ae
Axect <edeftg@gmail.com>
2017-07-25 10:42:39 +0900

    Prettify
    Former-commit-id: dc13f619fd40633427853d10a7162a5897adda13

--- CS/prob3/cmd/main.go
+++ CS/prob3/cmd/main.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"runtime"
-	"sync"
 
 	"github.com/Axect/Go/CS/prob3"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
 	runtime.GOMAXPROCS(20)
-	go func() {
-		orbit.DoOrbit()
-		defer wg.Done()
-	}()
-	wg.Wait()
 	orbit.DoOrbit()
 }
--- CS/prob3/orbit.go
+++ CS/prob3/orbit.go
@@ -25,6 +25,11 @@ type VList struct {
 	R [N + 1]Vector
 }
 
+type VPair struct {
+	r Vector
+	v Vector
+}
+
 func Initialize() (Vector, Vector) {
 	A := Vector{-9.851920196143998e-01 * AU, 1.316466809434336e-01 * AU, -4.877392224782687e-06 * AU}
 	B := Vector{-9.864337701483683e-01 * AU, 1.230799243164879e-01 * AU, -4.374019384763304e-06 * AU}
@@ -35,76 +40,74 @@ func (v Vector) String() string {
 	return fmt.Sprintf("%v, %v, %v", v.x, v.y, v.z)
 }
 
-func AS(v, w Vector, b bool) Vector {
-	var q Vector
-	if b {
-		q.x, q.y, q.z = v.x+w.x, v.y+w.y, v.z+w.z
-	} else {
-		q.x, q.y, q.z = v.x-w.x, v.y-w.y, v.z-w.z
-	}
-	return q
+func Plus(v, w Vector) Vector {
+	var A Vector
+	A.x = v.x + w.x
+	A.y = v.y + w.y
+	A.z = v.z + w.z
+	return A
 }
 
-func (v Vector) Mul(f float64) Vector {
-	var q Vector
-	q.x, q.y, q.z = v.x*f, v.y*f, v.z*f
-	return q
+func (c *Vector) Add(w Vector) {
+	c.x += w.x
+	c.y += w.y
+	c.z += w.z
+}
+
+func Mul(v Vector, f float64) Vector {
+	var A Vector
+	A.x = v.x * f
+	A.y = v.y * f
+	A.z = v.z * f
+	return A
 }
 
 func (V *VList) Assign(v Vector, i int) {
 	V.R[i] = v
 }
 
-func (v Vector) NormPow() (float64, float64) {
-	f := math.Pow(v.x, 2) + math.Pow(v.y, 2) + math.Pow(v.z, 2)
-	g := math.Sqrt(f)
-	return g, f
-}
-
-func Accel(coord Vector) Vector {
-	n, _ := coord.NormPow()
-	n = math.Pow(n, 3)
-	a := coord.Mul(-1. * G * M / n)
-	return a
+func (v Vector) Norm() float64 {
+	return math.Sqrt(math.Pow(v.x, 2) + math.Pow(v.y, 2) + math.Pow(v.z, 2))
 }
 
 func En(c, v Vector) (float64, float64) {
-	n, _ := c.NormPow()
-	_, p := v.NormPow()
-	T := 1. / 2. * m * p
-	U := (-1. * G * M * m) / n
+	T := 1. / 2. * m * math.Pow(v.Norm(), 2)
+	U := (-1. * G * M * m) / c.Norm()
 	return T, U
 }
 
+func (P *VPair) Run() {
+	a := Mul(P.r, -1.*G*M/math.Pow(P.r.Norm(), 3))
+	P.v.Add(Mul(a, tstep))
+	P.r.Add(Mul(P.v, tstep))
+}
+
 func Taylor(v1, v2 Vector) (VList, [N + 1]float64, [N + 1]float64) {
-	var c, v, a, NULL Vector
 	var C VList
 	var T, U [N + 1]float64
+	defer Time.TimeTrack(time.Now(), "Taylor")
 	t, u := &T, &U
 	i1, i2 := v1, v2
-	v0 := (AS(i2, i1, false)).Mul(1. / tstep)
-	c, v = i1, v0
-	C.Assign(c, 0)
-	_, u[0] = En(c, NULL)
-	c = AS(c, v.Mul(tstep), true)
-	C.Assign(c, 1)
-	_, u[1] = En(c, NULL)
+	v0 := Plus(Mul(v2, 1./tstep), Mul(v1, -1./tstep))
+	P, NULL := VPair{i2, v0}, Vector{0., 0., 0.}
+	C.Assign(i1, 0)
+	C.Assign(i2, 1)
+	_, u[0] = En(i1, NULL)
+	_, u[1] = En(i2, NULL)
 	for i := 2; i <= N; i++ {
-		a = Accel(c)
-		vf := v
-		v = AS(v, a.Mul(tstep), true)
-		vm := AS(v, vf, true)
+		vf := P.v
+		P.Run()
+		vm := Plus(Mul(vf, 0.5), Mul(P.v, 0.5))
 		if i == 2 {
-			vd := AS(vm.Mul(0.5), vf, false)
-			t[0], _ = En(NULL, AS(vf, vd, false))
+			vd := Plus(vm, Mul(vf, -1.))
+			t[0], _ = En(NULL, Plus(vf, Mul(vd, -1.)))
 		}
 		if i == N {
-			vd := AS(v, vm.Mul(0.5), false)
-			t[N], _ = En(NULL, AS(v, vd, true))
+			vd := Plus(P.v, Mul(vf, -1.))
+			t[N], _ = En(NULL, Plus(P.v, vd))
 		}
-		c = AS(c, v.Mul(tstep), true)
-		C.Assign(c, i)
-		t[i-1], u[i] = En(c, vm.Mul(0.5))
+		C.Assign(P.r, i)
+		t[i-1], u[i] = En(P.r, vm)
 	}
 	return C, T, U
 }
@@ -131,14 +134,10 @@ func ConvertF(F, G [N + 1]float64) [][]string {
 }
 
 func DoOrbit() {
-	//start := time.Now()
-	defer Time.TimeTrack(time.Now(), "Orbit")
 	C, _, _ := Taylor(Initialize())
 	D, _, _ := Taylor(Reversize(C))
-	ERR := AS(D.R[N], C.R[0], false)
-	//elapsed := time.Since(start)
-	fmt.Printf(" Number of years: %v\n Errors of Coordinates(AU): %v\n", N/730, ERR.Mul(1./AU))
-	//fmt.Println(T1[0], U1[0], T2[0], U2[0])
+	ERR := Plus(D.R[N], Mul(C.R[0], -1.))
+	fmt.Printf(" Number of years: %v\n Errors of Coordinates(AU): %v\n", N/730, Mul(ERR, 1./AU))
 	//W1 := Convert(C)
 	//W2 := Convert(D)
 	//W3 := ConvertF(T1, U1)
