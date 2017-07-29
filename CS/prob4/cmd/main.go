c063e94043b8feaf0ba58c89850c2ea88f351e6c
Axect <edeftg@gmail.com>
2017-07-29 14:03:11 +0900

    Great Channel!
    Former-commit-id: 0e6c78c8f8ba092b461869124324ba00e686b9ca

--- CS/prob4/cmd/main.go
+++ CS/prob4/cmd/main.go
@@ -2,23 +2,32 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"runtime"
+	"time"
 
 	"github.com/Axect/Go/CS/prob4"
+	"github.com/Axect/Go/Package/Time"
+)
+
+const (
+	n = 100
 )
 
 func main() {
+	runtime.GOMAXPROCS(20)
 	c := make(chan float64)
-	for i := 0; i < 10; i++ {
+	defer Time.TimeTrack(time.Now(), "Total Process")
+	for i := 0; i < n; i++ {
 		go func(chan float64) {
-			c <- pi.Pi2D(1000000) // Transfer pi value to channel
+			c <- pi.Pi2D(10000) // Transfer pi value to channel
 		}(c)
 	}
 
 	sum := 0.
 
-	for j := 0; j < 10; j++ {
-		sum += <-c / 10. // Transfer value in channel to sum
+	for j := 0; j < n; j++ {
+		sum += <-c / float64(n) // Transfer value in channel to sum
 	}
-	close(c)
-	fmt.Println(sum)
+	fmt.Printf("Approx pi: %v, Error: %v%%\n", sum, (math.Pi-sum)/math.Pi*100)
 }
