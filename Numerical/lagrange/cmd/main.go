44aa28d2ed07acd2cb4d98fe8c8c96b3a87c9b0d
Axect <edeftg@gmail.com>
2017-08-05 17:19:06 +0900

    Laplace to Lagrange
    Former-commit-id: bc628be75d3513719ad8e72007cbae4aed354038

--- Numerical/lagrange/cmd/main.go
+++ Numerical/lagrange/cmd/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	. "github.com/Axect/Go/Numerical/laplace"
+	. "github.com/Axect/Go/Numerical/Lagrange"
 )
 
 func main() {
 	xseq := []float64{1., 2., 3., 4.}
 	yseq := []float64{1., 4., 9., 16.}
 	p := LPolynomial(xseq, yseq)
-	fmt.Println(p(2.5))
+	fmt.Println(p(100))
 }
