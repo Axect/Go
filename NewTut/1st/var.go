6769329262dddc2dabbd9b44396cf063c2761fd2
Axect <edeftg@gmail.com>
2018-08-04 08:51:42 -0700

    Give Up
    Former-commit-id: 5a471ef65d1c6d6c757b7945624fd33519e9c41b

---
+++ NewTut/1st/var.go
@@ -0,0 +1,19 @@
+package main2 
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		name string
+		age	int
+		location	string
+	)
+
+	name = "Axect"
+	age = 25
+	location = "Gimpo"
+	
+	fmt.Println(name)
+}
